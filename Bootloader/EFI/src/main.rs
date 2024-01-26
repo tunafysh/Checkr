c093b830b852b519f2fed56a40537ff30e05e1e1
Trinitty999 <hanansmani@gmail.com>
2024-01-26 10:55:23 +0100

    finished build script.

--- Bootloader/EFI/src/main.rs
+++ Bootloader/EFI/src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 use uefi::prelude::*;
 use uefi::proto::console::text::Color;
 use uefi::proto::console::gop::{*, self};
@@ -18,7 +20,7 @@ fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     
     
     // Extract the number of rows and columns
-    let (width, height) = gop::ModeInfo::resolution();
+    // let (width, height) = gop::ModeInfo::resolution();
 
     system_table.boot_services().stall(10_000_000);
     // system_table.runtime_services().reset(ResetType::WARM, Status::SUCCESS, Option::None);
---
+++ linux_build.py
@@ -0,0 +1,31 @@
+import os, platform, sys, subprocess, shutil
+
+osname = platform.os.name
+
+os.path.exists("build")
+
+if osname == "posix":
+    print("Build started...")
+    os.mkdir("build")
+    os.mkdir("build/deps")
+    print("Building Bootloaders")
+    subprocess.Popen("cd Bootloader/BIOS && Makefile", shell=True)
+    subprocess.Popen("cd Bootloader/EFI && cargo build --target x86_64-unknown-uefi", shell=True)
+    print("Copying files")
+    shutil.copyfile("Bootloader/BIOS/boot.bin", "build/deps/boot.bin")
+    shutil.copyfile("Bootloader/EFI/target/x86_64-unknown-uefi/release/checkr-efi.efi", "build/deps/bootx64.efi")
+    shutil.copyfile("Installer/Linux/install", "build/install.sh")
+    print("Packing it up")
+    shutil.make_archive("Checkr", 'zip', "build")
+    print("Cleaning it up")
+    os.rmdir("build")
+    os.remove("Bootloader/BIOS/boot.bin")
+    os.rmdir("Bootloader/EFI/target")
+    print("Build finished! The output file is: Checkr.zip")
+else:
+    print("Unsupported system. Use linux please.")
+    
+def cleanup():
+    os.rmdir("build")
+    os.remove("Bootloader/BIOS/boot.bin")
+    os.rmdir("Bootloader/EFI/target")
