6124aeaf156f037634a0c8f9ec7374ff7a032fde
trinitty999 <hanansmani@gmail.com>
2024-02-07 18:32:20 +0100

    updated

--- Bootloader/EFI/src/main.c
+++ Bootloader/EFI/src/main.c
@@ -1,4 +1,4 @@
-#include 
+#include <efi/efi.h>
 #include <efi/efilib.h>
 
 EFI_STATUS
@@ -7,8 +7,13 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *systable)
 {
     InitializeLib(ImageHandle, systable);
     
+    uefi_call_wrapper(systable->ConOut->ClearScreen, 1, systable->ConOut);
     Print(L"Hello, world!\n");
-
+    int i;
+    for (i = 0; i < 10; i++){
+        uefi_call_wrapper(systable->BootServices->Stall, 1, 1000000);
+    }
+    uefi_call_wrapper(systable->RuntimeServices->ResetSystem, 1, 4);
     
 
     return EFI_SUCCESS;
