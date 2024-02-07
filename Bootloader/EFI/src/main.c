b177544d87534b721365f81c748958a7a55cb60b
trinitty999 <hanansmani@gmail.com>
2024-02-07 19:20:57 +0100

    still testing.

--- Bootloader/EFI/src/main.c
+++ Bootloader/EFI/src/main.c
@@ -8,7 +8,7 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *systable)
     InitializeLib(ImageHandle, systable);
     
     uefi_call_wrapper(systable->ConOut->ClearScreen, 1, systable->ConOut);
-    Print(L"Hello, world!\n");
+    uefi_call_wrapper(systable->ConOut->OutputString, 1, "Teststr");
     int i;
     for (i = 0; i < 10; i++){
         uefi_call_wrapper(systable->BootServices->Stall, 1, 1000000);
