095618926ec89685a4f69368fc22a89b00752012
Trinitty999 <hanansmani@gmail.com>
2024-02-08 13:33:12 +0100

    Finished EFI code.

--- Bootloader/EFI/src/main.c
+++ Bootloader/EFI/src/main.c
@@ -2,19 +2,27 @@
 #include <efi/efilib.h>
 
 EFI_STATUS
-EFIAPI
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *systable)
-{
-    InitializeLib(ImageHandle, systable);
-    
-    uefi_call_wrapper(systable->ConOut->ClearScreen, 1, systable->ConOut);
-    uefi_call_wrapper(systable->ConOut->OutputString, 1, "Teststr");
-    int i;
-    for (i = 0; i < 10; i++){
-        uefi_call_wrapper(systable->BootServices->Stall, 1, 1000000);
-    }
-    uefi_call_wrapper(systable->RuntimeServices->ResetSystem, 1, 4);
-    
+EFIAPI 
 
+ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    InitializeLib(ImageHandle, SystemTable);
+
+    // Set the foreground and background colors
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
+
+    // Get the screen dimensions
+    UINTN columns, rows;
+    ST->ConOut->QueryMode(ST->ConOut, ST->ConOut->Mode->Mode, &columns, &rows);
+
+    // Calculate the position to print "Checkmate" in the center
+    UINTN xPos = (columns - 9) / 2; // 9 is the length of "Checkmate"
+    UINTN yPos = rows / 2;
+
+    // Print "Checkmate" in the center
+    Print(L"%*sCheckmate", xPos, L"");
+
+    // Wait for a key press before exiting
+    WaitForSingleEvent(ST->ConIn->WaitForKey, 0);
+    
     return EFI_SUCCESS;
 }
