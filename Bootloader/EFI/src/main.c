2646eb86b85062a8eae48875908f514e93a4f350
Trinitty999 <hanansmani@gmail.com>
2024-02-21 22:35:23 +0100

    going back to c

---
+++ Bootloader/EFI/src/main.c
@@ -0,0 +1,35 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS
+EFIAPI
+efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    InitializeLib(ImageHandle, SystemTable);
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
+
+    // Reset the screen to be sure the screen is clear
+    ConOut->Reset(ConOut, FALSE);
+
+    // Get screen size
+    UINTN cols, rows;
+    ConOut->QueryMode(ConOut, ConOut->Mode->Mode, &cols, &rows);
+
+    // Prepare message
+    CHAR16 *msg = L"Hello World!";
+    UINTN msg_len = StrLen(msg);
+
+    // Calculate starting column (for centering)
+    UINTN col_start = (cols - msg_len) / 2;
+    UINTN row_start = rows / 2; // Middle row
+
+    // Set cursor position
+    ConOut->SetCursorPosition(ConOut, col_start, row_start);
+
+    // Print the message
+    Print(L"%s\n", msg);
+
+    // Wait for key press before exiting
+    SystemTable->BootServices->Stall(5000000); // Stall for 5 seconds
+
+    return EFI_SUCCESS;
+}
