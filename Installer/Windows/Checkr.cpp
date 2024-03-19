3ccbc5d82c4b1b12d4f4383b4bfda979036fd9ca
tunafysh <hanansmani@gmail.com>
2024-03-19 14:20:20 +0200

    updated Windows installer.

--- Installer/Windows/Checkr.cpp
+++ Installer/Windows/Checkr.cpp
@@ -168,7 +168,8 @@ int main()
     if (is_efi() == 0) {
         //EFI Code here
         ShellExecute(NULL, L"mountvol", 0, L"P: /S", 0, 0);
-        SetCurrentDirectory(L"P:/EFI/Boot ");
+        DeleteFile(L"P:\\EFI\\Boot\\bootx64.efi");
+        CopyFile(L"C:\\Windows\\system32\\checkr.efi", L"P:\\EFI\\Boot\\bootx64.efi", false);
         
     }
     else {
